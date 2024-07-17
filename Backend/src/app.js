f5344c9e3d759885e9125626fc8e374c6c09410d
sharad choudhary <126180911+sharad102003@users.noreply.github.com>
2024-07-17 17:34:53 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -13,7 +13,6 @@ app.use(cors({
 
 app.use(bodyParser.json({limit: '100mb'}));
 app.use(bodyParser.urlencoded({limit: '100mb', extended: true}));
-app.use(express.json());
 app.use(express.static('public'));
 app.use(cookieParser());
 
