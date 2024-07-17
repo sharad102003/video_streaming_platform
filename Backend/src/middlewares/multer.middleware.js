55dfb38808f5fa9fd37305aa6a642a1fae94e628
sharad choudhary <126180911+sharad102003@users.noreply.github.com>
2024-07-17 17:10:57 +0530

    Update multer.middleware.js

--- Backend/src/middlewares/multer.middleware.js
+++ Backend/src/middlewares/multer.middleware.js
@@ -22,7 +22,7 @@ const storage = multer.diskStorage({
 export const upload = multer({
   storage,
   limits: {
-    fileSize: 50 * 1024 * 1024, // 50 MB file size limit (adjust as needed)
+    fileSize: 150 * 1024 * 1024, // 50 MB file size limit (adjust as needed)
   },
 
 });
