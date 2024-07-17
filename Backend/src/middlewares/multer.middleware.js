fb7759859311c049e86e966a7f456862cc055bf3
sharad choudhary <126180911+sharad102003@users.noreply.github.com>
2024-07-17 12:31:27 +0530

    Update multer.middleware.js

--- Backend/src/middlewares/multer.middleware.js
+++ Backend/src/middlewares/multer.middleware.js
@@ -1,15 +1,24 @@
-import multer from "multer";
+import multer from 'multer';
+import fs from 'fs';
+import path from 'path';
+
+// Ensure the public/temp directory exists
+const localDir = './public/temp';
+const tempDir = process.env.NODE_ENV === 'production' ? '/tmp' : localDir;
+
+if (!fs.existsSync(tempDir)) {
+  fs.mkdirSync(tempDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, "./public/temp")
-    },
-    filename: function (req, file, cb) {
-      
-      cb(null, file.originalname)
-    }
-  })
-  
-export const upload = multer({ 
-    storage, 
-})
+  destination: function (req, file, cb) {
+    cb(null, tempDir);
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+
+export const upload = multer({
+  storage,
+});
