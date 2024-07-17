9e0f02f93bd602aefd00a50033cd05a1088ea3b4
sharad choudhary <126180911+sharad102003@users.noreply.github.com>
2024-07-17 17:41:45 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -11,9 +11,9 @@ app.use(cors({
   credentials: true
 }));
 
-app.use(bodyParser.json({limit: '100mb'}));
-app.use(bodyParser.urlencoded({limit: '100mb', extended: true}));
-app.use(express.static('public'));
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
 app.use(cookieParser());
 
 // Import routes
